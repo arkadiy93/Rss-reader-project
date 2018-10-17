efdacd6c5fce36c366e3183f7743a17ccb6c541c
Arkadiy Stepanov <nvriper@gmail.com>
2018-10-17 14:28:14 +0200

    add modal plugin

--- src/init.js
+++ src/init.js
@@ -2,25 +2,56 @@ import axios from 'axios';
 import isURL from 'validator/lib/isURL';
 import { find } from 'lodash';
 import { watch } from 'melanke-watchjs';
+import $ from 'jquery';
 
 export default () => {
   const state = {
     feedURLs: [],
     feedList: [],
+    modalData: { isOpen: false, data: '' },
   };
+
+  const handleClick = ({ description }) => () => {
+    state.modalData = { isOpen: true, data: description };
+  };
+
+  $('#modal').on('hide.bs.modal', () => {
+    state.modalData = { isOpen: false, data: '' };
+  });
+
+  watch(state, 'modalData', () => {
+    const modalBody = document.querySelector('.modal-body');
+    if (state.modalData.isOpen) {
+      const p = document.createElement('p');
+      p.innerText = state.modalData.data;
+      modalBody.append(p);
+      $('#modal').modal('show');
+    } else {
+      modalBody.innerHTML = '';
+    }
+  });
 
   watch(state, 'feedList', () => {
     const jumbotron = document.querySelector('.jumbotron');
     const { itemList } = state.feedList[0];
     const ul = document.createElement('ul');
+    const hr = document.createElement('hr');
     itemList.forEach((item) => {
       const li = document.createElement('li');
       const a = document.createElement('a');
+      const button = document.createElement('button');
+      button.classList.add('btn', 'btn-primary');
+      button.setAttribute('type', 'button');
+      button.setAttribute('data-whatever', 'lol');
+      button.innerHTML = 'Open';
+      button.addEventListener('click', handleClick(item));
       a.setAttribute('href', item.link);
       a.innerHTML = item.title;
       li.append(a);
+      li.append(button);
       ul.append(li);
     });
+    jumbotron.append(hr);
     jumbotron.append(ul);
   });
 
@@ -49,8 +80,9 @@ export default () => {
       const items = channel.getElementsByTagName('item');
       const itemList = Array.from(items).map((item) => {
         const itemTitle = item.querySelector('title').innerHTML;
+        const itemDescription = item.querySelector('description').innerHTML;
         const link = item.querySelector('guid').innerHTML;
-        return { title: itemTitle, link };
+        return { title: itemTitle, link, description: itemDescription };
       });
       state.feedList = [{ title, description, itemList }, ...state.feedList];
     }).catch((err) => {
